fe959207b39563470af9619a5fcb873021b7d75f
scarfacedotcom <peteradaaku@gmail.com>
2022-07-28 05:29:23 +0100

    setting routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+// page components
+import Home from './pages/home/Home'
+import Login from './pages/login/Login'
+import Signup from './pages/signup/Signup'
+
+//styles
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
+        </Routes>
+      </Router>
       
     
     </div>
