4b7040ca59a78fad0becef64d1232cc517874d59
scarfacedotcom <peteradaaku@gmail.com>
2022-08-01 07:25:08 +0100

    creating the context hook

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -2,3 +2,14 @@ import { createContext, useReducer } from "react";
 
 export const AuthContext = createContext()
 
+const [state, dispatch] = useReducer(authReducer, {
+  user: null
+})
+
+export const AuthContextProvider = ({ children }) => {
+  return (
+    <AuthContext.Provider>
+      { children }
+    </AuthContext.Provider>
+  )
+}
