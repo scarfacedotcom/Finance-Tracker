cec53646618fc94d9c0718e85c5e2023f9d6e541
scarfacedotcom <peteradaaku@gmail.com>
2022-08-02 07:36:13 +0100

    dispatch login action

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -4,19 +4,21 @@ export const AuthContext = createContext()
 
 export const authReducer = (state, action) => {
   switch (action.type) {
-    
+    case 'LOGIN' :
+      return { ...state, user: action.payload }
     default:
       return state
   }
 }
 
-const [state, dispatch] = useReducer(authReducer, {
-  user: null
-})
 
-dispatch({ type: 'LOG_IN' })
 
 export const AuthContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(authReducer, {
+    user: null
+  })
+  console.log('AuthContext state:', state)
+  
   return (
     <AuthContext.Provider value = {{ ...state, dispatch }}>
       { children }
