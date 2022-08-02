83533b9f30256ecc403739a3fb5faf2ff4b03975
scarfacedotcom <peteradaaku@gmail.com>
2022-08-02 06:13:56 +0100

    few changes

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -2,13 +2,23 @@ import { createContext, useReducer } from "react";
 
 export const AuthContext = createContext()
 
+export const authReducer = (state, action) => {
+  switch (action.type) {
+    
+    default:
+      return state
+  }
+}
+
 const [state, dispatch] = useReducer(authReducer, {
   user: null
 })
 
+dispatch({ type: 'LOG_IN' })
+
 export const AuthContextProvider = ({ children }) => {
   return (
-    <AuthContext.Provider>
+    <AuthContext.Provider value = {{ ...state, dispatch }}>
       { children }
     </AuthContext.Provider>
   )
