a5c68e06bf7d9dd929624331547fe8dc7465befa
scarfacedotcom <peteradaaku@gmail.com>
2022-08-02 10:25:50 +0100

    few changes

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -6,6 +6,8 @@ export const authReducer = (state, action) => {
   switch (action.type) {
     case 'LOGIN' :
       return { ...state, user: action.payload }
+    case 'LOGOUT' :
+      return { ...state, user: null }
     default:
       return state
   }
@@ -18,7 +20,7 @@ export const AuthContextProvider = ({ children }) => {
     user: null
   })
   console.log('AuthContext state:', state)
-  
+
   return (
     <AuthContext.Provider value = {{ ...state, dispatch }}>
       { children }
--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -29,4 +29,6 @@ const useLogout = () => {
       setIsPending(false)
     }
   }
+
+  return { logout, error, isPending }
 }
