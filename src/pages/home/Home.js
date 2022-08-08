4b5854f6eb084966972a9ad5ce33949517b35765
scarfacedotcom <peteradaaku@gmail.com>
2022-08-08 22:40:05 +0100

    code for trx form

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -3,8 +3,13 @@ import styles from './Home.module.css'
 
 export default function Home() {
   return (
-    <div>
-      Home
+    <div className={styles.container} >
+      <div className={styles.content}>
+        transaction List
+      </div>
+      <div className={styles.sidebar}>
+
+      </div>
     </div>
   )
 }
---
+++ src/pages/home/TransactionForm.js
@@ -0,0 +1,13 @@
+import { useState } from "react"
+
+
+export default function TransactionForm() {
+  const [name, setName] = useState('')
+  const [amount, setAmount] = useState('')
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
