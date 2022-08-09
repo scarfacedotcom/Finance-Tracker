c7feb595e553a5ea22b33beed66a5b43233a4dc7
scarfacedotcom <peteradaaku@gmail.com>
2022-08-09 23:18:06 +0100

    few changes

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <div className={styles.container} >
       <div className={styles.content}>
-        transaction List
+        Transaction List
       </div>
       <div className={styles.sidebar}>
         <TransactionForm />
