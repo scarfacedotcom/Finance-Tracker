80558c7c051315ecf3a8ee46485732a0f68647b3
scarfacedotcom <peteradaaku@gmail.com>
2022-08-12 06:42:40 +0100

    fire store hook

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -1,6 +1,21 @@
 import { useReducer, useState, useEffect } from "react";
 import { projectFireStore } from "../firebase/config";
 
-export const useFirestore = (collection) => {
+let initialState = {
+  document: null,
+  isPending: false,
+  error: null,
+  success: null
+}
 
+const firestoreReducer = (state, action) => {
+  switch (action.type) {
+
+    default:
+      return state
+  }
+}
+
+export const useFirestore = (collection) => {
+  const [response, dispatch] = useReducer(firestoreReducer, initialState)
 }
