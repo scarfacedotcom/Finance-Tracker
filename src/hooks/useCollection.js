78f27f9616e2cd624eb95d927e37b1fe5b85789a
scarfacedotcom <peteradaaku@gmail.com>
2022-08-18 20:52:46 +0100

    few changes

--- src/hooks/useCollection.js
+++ src/hooks/useCollection.js
@@ -8,7 +8,15 @@ export const useCollection = (collection) => {
 
   useEffect(() => {
     let ref = projectFireStore.collection(collection)
-    
+
+    const unsub = ref.onSnapshot((snapshot) => {
+      let results = []
+
+      snapshot.docs.forEach(doc => {
+        results.push({...doc, id:doc.id})
+      })
+    })
+
   }, [collection])
 
 }
